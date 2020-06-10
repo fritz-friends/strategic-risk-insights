7440b1ac4639d5b24c0afbcbdfbe7e73f223d65a
Noah Read <noahread@gmail.com>
2020-06-09 21:35:24 -0700

    Navbar Adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import logo from '../img/logo.svg';
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
+      navBarActiveClass: ""
     };
-  };
+  }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-            navBarActiveClass: 'is-active',
-          })
+              navBarActiveClass: "is-active"
+            })
           : this.setState({
-            navBarActiveClass: '',
-          })
+              navBarActiveClass: ""
+            });
       }
     );
   };
@@ -41,7 +41,11 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" title="Logo">
-              <img src={logo} alt="SRI" style={{ width: '147px', maxHeight: 'none' }} />
+              <img
+                src={logo}
+                alt="SRI"
+                style={{ width: "147px", maxHeight: "none" }}
+              />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -59,28 +63,47 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/services">
+              <Link
+                className="navbar-item"
+                to="/services"
+                activeClassName="current-page"
+              >
                 Services
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link
+                className="navbar-item"
+                to="/blog"
+                activeClassName="current-page"
+              >
                 Blog
               </Link>
-              <Link className="navbar-item" to="/podcast">
+              <Link
+                className="navbar-item"
+                to="/podcast"
+                activeClassName="current-page"
+              >
                 Podcast
               </Link>
-              <Link className="navbar-item" to="/book">
+              <Link
+                className="navbar-item"
+                to="/book"
+                activeClassName="current-page"
+              >
                 Book
               </Link>
-              <Link className="navbar-item" to="/about">
+              <Link
+                className="navbar-item"
+                to="/about"
+                activeClassName="current-page"
+              >
                 About
               </Link>
             </div>
-
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
 export default Navbar;
