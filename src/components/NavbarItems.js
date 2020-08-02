e2184cdf1474d7e8f835e3c43a481e44b0bd1baf
James 'Mac' Read <james.macgregor.read@gmail.com>
2020-08-02 10:32:18 -0600

    tweaks after customer preview

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,31 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
+import Login from "./Login";
 
 const NavbarItems = ({ className }) => {
 	return (
 		<>
-			<Link className={className + " btn btn-nav"} to="/services" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/services"
+				activeClassName="current-page"
+			>
 				Services
 			</Link>
-			<Link className={className + " btn btn-nav"} to="/cases" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/cases"
+				activeClassName="current-page"
+			>
 				Cases
 			</Link>
-			<Link className={className + " btn btn-nav"} to="/blog" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/blog"
+				activeClassName="current-page"
+			>
 				Blog
 			</Link>
-			<Link className={className + " btn btn-nav"} to="/podcast" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/podcast"
+				activeClassName="current-page"
+			>
 				Podcast
 			</Link>
-			<Link className={className + " btn btn-nav"} to="/book" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/book"
+				activeClassName="current-page"
+			>
 				Book
 			</Link>
-			<Link className={className + " btn btn-nav"} to="/about" activeClassName="current-page">
+			<Link
+				className={className + " btn btn-nav"}
+				to="/about"
+				activeClassName="current-page"
+			>
 				About
 			</Link>
-			<Link className={className + " btn btn-nav btn-nav--secondary secondary-nav-item"} to="/client-login" activeClassName="current-page">
-				Client Login
-			</Link>
+			<Login className={className} />
 		</>
 	);
 };
