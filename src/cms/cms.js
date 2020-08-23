9e466e32ea101192ecaefb0dc1191eb67b87e4b9
James 'Mac' Read <james.macgregor.read@gmail.com>
2020-08-22 20:04:46 -0600

    Index and cms changes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,4 @@
+import React from "react";
 import CMS from "netlify-cms-app";
 // import uploadcare from "netlify-cms-media-library-uploadcare";
 // import cloudinary from "netlify-cms-media-library-cloudinary";
@@ -18,3 +19,47 @@ CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("podcast", PodcastPreview);
 CMS.registerPreviewTemplate("book", BookPreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
+
+const video = {
+	label: "Video",
+	id: "video",
+	fromBlock: (match) =>
+		match && {
+			video: match[2],
+			alt: match[1],
+		},
+	toBlock: ({ alt, video }) => `![${alt || ""}](${video || ""})`,
+	// eslint-disable-next-line react/display-name
+	toPreview: ({ alt, video }, getAsset, fields) => {
+		console.log("Are we making it here?");
+		const videoField = fields?.find((f) => f.get("name") === "video");
+		console.log("videoField :>> ", videoField);
+		const src = getAsset(video, videoField);
+		console.log("src :>> ", src);
+		// return (
+		// 	<>
+		// 		<video poster={src || ""} controls alt={alt || ""}>
+		// 			<source src={src || ""} type="video/mp4" />
+		// 		</video>
+		// 	</>
+		// );
+		return <p>Hello There</p>;
+	},
+	pattern: /^!\[(.*)\]\((.*?)(\s"(.*)")?\)$/,
+	fields: [
+		{
+			label: "Video",
+			name: "video",
+			widget: "file",
+			media_library: {
+				allow_multiple: false,
+			},
+		},
+		{
+			label: "Alt Text",
+			name: "alt",
+		},
+	],
+};
+
+CMS.registerEditorComponent(video);
